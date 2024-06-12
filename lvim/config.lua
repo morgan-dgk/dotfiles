6ff8f3f299f3ce4d11186b9a8b14508cf697c380
morgan-dgk <mdgk95@proton.me>
2024-06-12 20:48:00 +1000

    update folding and remove trailing chars from folds

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,9 +4,25 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 
+lvim.plugins = {
+  { "HiPhish/rainbow-delimiters.nvim" },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead"
+  }
+}
+
+vim.opt.fillchars = { fold = " ", }
 -- Toggle relative line number
 lvim.keys.normal_mode["<C-L><C-L>"] = ":set invrelativenumber<CR>"
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "1"
+vim.opt.foldtext = require('foldtext')
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
 -- Disable tsserver
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
 
