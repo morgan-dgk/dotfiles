8cd7214ef7bf6f70426fecc0072c665e7fb29d54
morgan-dgk <mdgk95@proton.me>
2024-06-17 14:49:21 +1000

    reenable tsserver for vanilla js files

--- lvim/config.lua
+++ lvim/config.lua
@@ -18,13 +18,11 @@ lvim.keys.normal_mode["<C-L><C-L>"] = ":set invrelativenumber<CR>"
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldcolumn = "1"
+vim.opt.foldcolumn = "0"
 vim.opt.foldtext = require('foldtext')
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 1
 vim.opt.foldnestmax = 4
--- Disable tsserver
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
 
 -- Automatically changed to pwd
 vim.opt.autochdir = true
