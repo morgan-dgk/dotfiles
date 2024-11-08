dc64cef849bbd73738eb7f84a782a48585295482
Morgan Kerle <mdgk95@proton.me>
2024-11-09 10:31:56 +1100

    🎨 use "structured setup" for lazy plugins

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,9 +4,6 @@ require "keys.main"           -- Keys for built-in features
 require "keys.plugins"        -- Keys for plugged features
 
 require "plugins.core.use"    -- Built-in plugins in use
-require "plugins.user.config" -- Configs for built-in plugins
-require "plugins.user.use"    -- User plugins being used
-require "plugins.user.config" -- Configs for user plugins
 
 vim.opt.fillchars = { fold = " ", }
 
@@ -18,8 +15,7 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 1
 vim.opt.foldnestmax = 4
 
--- Automatically changed to pwd
-vim.opt.autochdir = true
+lvim.plugins = {{ import = "plugins.user" }} -- Load user plugins from plugins/users directory
 
 --format on save
 lvim.format_on_save.enabled = true
---
+++ lvim/lua/plugins/user/vimflog.lua
@@ -0,0 +1,6 @@
+return {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = { "tpope/vim-fugitive" }
+}
