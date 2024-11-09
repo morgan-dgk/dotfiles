e4dcd47f975093764de6ec5e5c4d52d2a9b9f87b
Morgan Kerle <mdgk95@proton.me>
2024-11-09 21:05:22 +1100

    add termtoggle config

--- lvim/lua/keys/plugins.lua
+++ lvim/lua/keys/plugins.lua
@@ -1,3 +1,19 @@
+-- ToggleTerminal
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
+-- ToggleTerminal - Map regular window commands to terminal mode
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
+vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
+vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
+vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
+
 -- Gitmojis telescope picker
 lvim.builtin.which_key.mappings["sG"] = { "<cmd>lua require'telescope.builtin'.symbols{}<CR>", "Gitmoji" }
 
@@ -8,5 +24,4 @@ vim.keymap.set("n", "gpd", "<cmd>lua require('goto-preview').goto_preview_defini
 vim.keymap.set("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>)",
   { desc = "Preview implementationm" })
 
-vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", { desc = "Close all windows"})
-
+vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", { desc = "Close all windows" })
--- lvim/lua/plugins/core/init.lua
+++ lvim/lua/plugins/core/init.lua
@@ -1,3 +1,4 @@
 require "plugins.core.disabled"
 require "plugins.core.nvimtree"
 require "plugins.core.whichkey"
+require "plugins.core.termtoggle"
