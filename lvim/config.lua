638f407fe610bfff1903cdeb9c31de78634e7115
Morgan Kerle <mdgk95@proton.me>
2024-11-09 13:32:04 +1100

    update main config

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,14 +1,15 @@
 -- Config structure from https://stackoverflow.com/questions/74816218/managing-lunar-vim-lvim-config-lua-by-separating-it-in-different-files
 
-require "base.other"
+require "base.visual"                          --  Set colourscheme or statusline appearance here
+require "base.other"                           --  Miscellaneous configs
 
-require "keys.main"           -- Keys for built-in features
-require "keys.plugins"        -- Keys for plugged features
+require "keys.main"                            -- Keys for built-in features
+require "keys.plugins"                         -- Keys for plugged features
 
-require "plugins.core.use"    -- Built-in plugins in use
+require "plugins.core"                         -- core plugins config
 
-lvim.plugins = {{ import = "plugins.user" }} -- Load user plugins from plugins/users directory
+lvim.plugins = { { import = "plugins.user" } } -- Load user plugin configs
 
 --format on save
 lvim.format_on_save.enabled = true
-lvim.format_on_save.pattern = { "*.py", "*.rs", "*.ts", "*.mjs", "*.js", "*.cjs", "*.html", "*.json" }
+lvim.format_on_save.pattern = { "*.lua", "*.py", "*.rs", "*.ts", "*.mjs", "*.js", "*.cjs", "*.html", "*.json" }
