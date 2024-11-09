713ea27b30f7efa19ddfabe8bf7ff35dabae98e6
Morgan Kerle <mdgk95@proton.me>
2024-11-09 13:30:12 +1100

    🚸 Use vim.keymap.set
    This allows adding a description to keymaps which can be searched

--- lvim/lua/keys/plugins.lua
+++ lvim/lua/keys/plugins.lua
@@ -1,1 +1,12 @@
+-- Gitmojis telescope picker
 lvim.builtin.which_key.mappings["sG"] = { "<cmd>lua require'telescope.builtin'.symbols{}<CR>", "Gitmoji" }
+
+-- goto-preview
+vim.keymap.set("n", "gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+  { desc = "Preview definition" })
+
+vim.keymap.set("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>)",
+  { desc = "Preview implementationm" })
+
+vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", { desc = "Close all windows"})
+
